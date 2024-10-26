420233a9eb3a10d4a682dc35f1fe4e78b9605ecf
eloanapontes <eloana.pontes@gmail.com>
2024-10-26 18:18:52 -0300

    primeiro commit

---
+++ src/Player.java
@@ -0,0 +1,37 @@
+public class Player {
+    private String nome;
+    private int id;
+    private String[] inventario;
+
+    public Player(String[] inventario, int id, String nome) {
+        this.inventario = inventario;
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String[] getInventario() {
+        return inventario;
+    }
+
+    public void setInventario(String[] inventario) {
+        this.inventario = inventario;
+    }
+}
+
+
